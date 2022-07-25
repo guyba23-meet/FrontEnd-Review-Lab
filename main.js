86a5943be41759f52c0d479efb0bba58144f5ff9
guyba23-meet <109900022+guyba23-meet@users.noreply.github.com>
2022-07-25 16:59:17 +0300

    WORKS

---
+++ main.js
@@ -0,0 +1,5 @@
+function changeBackgroundColor(color){
+	//var bg_color = document.getElementsByTag("body");
+	document.body.style.backgroundColor = "green";
+	//bg_color.style.backgroundColor = color;
+}
